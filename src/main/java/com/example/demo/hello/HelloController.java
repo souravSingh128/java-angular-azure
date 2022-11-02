404741d67ffb77f24547f277868d310d3326ee0f
Sourav Singh <sourav.singh@winfosolutions.com>
2022-11-02 16:27:28 +0530

    first-commit

---
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String sayHi() {
+		System.out.println("sourav singh");
+		return "hi Sourav";
+	}
+}
